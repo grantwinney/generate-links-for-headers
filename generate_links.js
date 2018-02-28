c945a1335434e8ade87c4fb14386ea2439cea1d1
Grant Winney <grantwinney@gmail.com>
2018-02-28 13:07:19 -0500

    show popup when chain icon clicked

--- generate_links.js
+++ generate_links.js
@@ -1,11 +1,30 @@
-var copyFunc = function copyHeaderLinkToClipboard(text) {
+var copyFunc = function copyHeaderLinkToClipboard(event) {
+    var text= 'ahasdf';
     if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
         var textarea = document.createElement("textarea");
         textarea.textContent = text;
         document.body.appendChild(textarea);
         textarea.select();
+        event.target.style.border = "1px solid rgba(255,0,0,1)";
         try {
-            return document.execCommand("copy");
+            var result = document.execCommand("copy");
+            
+            var tooltip = document.getElementById('glfh_tooltip');
+            tooltip.style.top = (event.pageY + 5) + 'px';
+            tooltip.style.left = (event.pageX + 5) + 'px';
+            tooltip.style.display = "block";
+
+            var timer = setInterval(function () {
+                var op = 1;  // initial opacity
+                if (op <= 0.1){
+                    clearInterval(timer);
+                }
+                tooltip.style.opacity = op;
+                tooltip.style.filter = 'alpha(opacity=' + op * 100 + ")";
+                op += op / 0.1;
+            }, 10);
+
+            return result;
         } catch (ex) {
             console.warn("Copy to clipboard failed.", ex);
             return false;
@@ -15,14 +34,23 @@ var copyFunc = function copyHeaderLinkToClipboard(text) {
     }
 };
 
+// Inject a new element into the page
+var notice = document.createElement('div');
+notice.innerHTML = "COPIED!";
+notice.id = "glfh_tooltip";
+notice.style.display = "none";
+notice.style.position = "absolute";
+notice.style.setProperty("border-radius", "5px");
+document.body.appendChild(notice);
+
 // Inject the above function into the current page
 var script = document.createElement('script');
 script.textContent = copyFunc;
 (document.head||document.documentElement).appendChild(script);
 
-// Select all headers with a valid ID value
-var selector = 'h1[id]:not([id=""]), h2[id]:not([id=""]), h3[id]:not([id=""]), h4[id]:not([id=""]), h5[id]:not([id=""]), h6[id]:not([id=""])';
-var headers = document.querySelectorAll(selector);
+// Select all anchors with a valid ID value
+var selector = 'h1[id]:not([id=""]), h2[id]:not([id=""]), h3[id]:not([id=""]), h4[id]:not([id=""]), h5[id]:not([id=""]), h6[id]:not([id=""])';  // , a[name]:not([name=""])
+var anchors = document.querySelectorAll(selector);
 
 // The 'chain' SVG element
 var chainsvg = "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 " +
@@ -32,9 +60,9 @@ var chainsvg = "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 
 // Rebuild the URL, omitting any existing anchors
 var baseUrl = location.protocol + '//' + location.host + location.pathname + location.search + '#';
 
-// Iterate through the headers, adding a link to the left of each one
-headers.forEach(function(header) {
-    var url = baseUrl + header.getAttribute("id");
-    var link = '<a href="javascript:copyHeaderLinkToClipboard(\'' + url + '\')" title="Copy link to clipboard"><svg height="16" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="' + chainsvg + '"></path></svg></a>';
-    header.innerHTML = '<div class="glfh_headerContainer"><div class="glfh_linkContainer">' + link + '</div><div class="glfh_textContainer">' + header.innerHTML + '</div></div>';
+// Iterate through the anchors, adding a link to the left of each one
+anchors.forEach(function(anchor) {
+    var url = baseUrl + anchor.getAttribute("id");
+    var link = '<a onclick="copyHeaderLinkToClipboard(event)" href="#" title="Copy link to clipboard"><svg height="16" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="' + chainsvg + '"></path></svg></a>';
+    anchor.innerHTML = '<div class="glfh_headerContainer"><div class="glfh_linkContainer">' + link + '</div><div class="glfh_textContainer">' + anchor.innerHTML + '</div></div>';
 });
