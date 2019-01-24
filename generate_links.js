abc0ace9b86df20aca72f9bb6a371e5f32c06dc1
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2019-01-24 15:43:14 -0500

    look for parent id if not found in children; swap onclick for addeventlistener

--- generate_links.js
+++ generate_links.js
@@ -1,11 +1,11 @@
 // Copies the link to the clipboard and displays a small colored box so you know something happened
-var copyFunc = function copyHeaderLinkToClipboard(event, text) {
+let copyFunc = function copyHeaderLinkToClipboard(event, text) {
     if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-        var textarea = document.createElement("textarea");
+    let textarea = document.createElement("textarea");
         textarea.textContent = text;
         document.body.appendChild(textarea);
         textarea.select();
-        var svgElement = (event.target.tagName === "A") ? event.target.childNodes[0] : event.target;
+        let svgElement = (event.target.tagName === "A") ? event.target.childNodes[0] : event.target;
         try {
             document.execCommand("copy");
             svgElement.style.border = "1px solid rgba(0,200,0,1)";
@@ -29,8 +29,8 @@ function getFirstId(element) {
     } else if (element.hasAttribute("name")) {
         return element.getAttribute("name");
     } else if (element.hasChildNodes()) {      
-        for (var i = 0; i < element.children.length; i++) {
-            var id = getFirstId(element.children[i]);
+        for (let i = 0; i < element.children.length; i++) {
+            let id = getFirstId(element.children[i]);
             if (id !== undefined) {
                 return id;
             }
@@ -41,29 +41,51 @@ function getFirstId(element) {
     }
 }
 
+// Check the element's immediate parent, in search of an appropriate ID to link to
+// Return the ID if found; otherwise return undefined
+function getParentId(element) {
+    let pNode = element.parentNode;
+    if (pNode !== undefined && (pNode.tagName === 'A' || pNode.tagName === 'DIV')) {
+        if (pNode.hasAttribute("id")) {
+            return pNode.getAttribute("id");
+        } else if (pNode.hasAttribute("name")) {
+            return pNode.getAttribute("name");
+        } else {
+            return undefined;
+        }
+    } else {
+        return undefined;
+    }
+}
+
 // The 'chain' SVG element
-var chainsvg = "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 " +
+let chainsvg = "M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 " +
                "4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 " +
                "1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z";
 
 // Rebuild the URL, omitting any existing headers
-var baseUrl = location.protocol + '//' + location.host + location.pathname + location.search + '#';
+let baseUrl = location.protocol + '//' + location.host + location.pathname + location.search + '#';
 
 // Select all anchors with a valid ID value
-var headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+let headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
 // Inject the 'copy link' function into the current page, if there are any headers
 if (headers.length > 0) {
-    var script = document.createElement('script');
+    let script = document.createElement('script');
     script.textContent = copyFunc;
     (document.head||document.documentElement).appendChild(script);
 }
 
 // Iterate through the headers, adding a link to the left of each one
+let counter = 0;
 headers.forEach(function(header) {
-    var id = getFirstId(header);
+    let id = getFirstId(header);
+    if (id === undefined) {
+        id = getParentId(header);
+    }
     if (id !== undefined) {
-        var link = '<a onclick="copyHeaderLinkToClipboard(event, \'' + baseUrl + id + '\')" href="javascript:;" title="Copy link to clipboard"><svg height="16" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="' + chainsvg + '"></path></svg></a>';
-        header.innerHTML = '<div class="glfh_headerContainer"><div class="glfh_linkContainer">' + link + '</div>' + header.innerHTML + '</div>';
+        let genlinkid='genlink_' + counter++;
+        header.innerHTML = '<div class="glfh_headerContainer"><div class="glfh_linkContainer"><a id="' + genlinkid + '" href="javascript:;" title="Copy link to clipboard"><svg height="16" viewBox="0 0 16 16" width="16"><path fill-rule="evenodd" d="' + chainsvg + '"></path></svg></a></div>' + header.innerHTML + '</div>';
+        document.getElementById(genlinkid).addEventListener("click", function(evt) { copyFunc(evt, baseUrl + id) });
     }
 });
