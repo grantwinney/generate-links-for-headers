e9a6cc90b9e45f81187929cddf8a0af4a9b589bf
Grant Winney <1354059+grantwinney@users.noreply.github.com>
2022-03-03 13:37:56 -0500

    run script after entire page loads, to not interfere with some other mdn script; fix bug #17

--- source/scripts/generate_links.js
+++ source/scripts/generate_links.js
@@ -30,38 +30,34 @@ function getParentId(element) {
     return undefined;
 }
 
-// Iterate through the headers, adding a link to the left of each one where possible
-let counter = 0;
-document.querySelectorAll('h1, h2, h3, h4, h5, h6')
-        .forEach(function(header) {
-            let id = getFirstId(header) || getParentId(header);
-            if (id) {
-                let anchorUrl = `${location.protocol}//${location.host}${location.pathname}${location.search}#${id}`;
-                let genlinkid = `genlink_${counter++}`;
-                let clonedHeaderNode = header.cloneNode(true);
+window.addEventListener("load", () => {
+    document.querySelectorAll('h1, h2, h3, h4, h5, h6')
+            .forEach(function(header) {
+                let id = getFirstId(header) || getParentId(header);
+                if (id) {
+                    let anchorUrl = `${location.protocol}//${location.host}${location.pathname}${location.search}#${id}`;
+                    let clonedHeaderNode = header.cloneNode(true);
 
-                let copyLink = document.createElement('a');
-                copyLink.id = genlinkid;
-                copyLink.href = anchorUrl;
-                copyLink.title = 'Copy link to clipboard';
-                copyLink.textContent = String.fromCodePoint(128279);
+                    let copyLink = document.createElement('a');
+                    copyLink.href = anchorUrl;
+                    copyLink.title = 'Copy link to clipboard';
+                    copyLink.textContent = String.fromCodePoint(128279);
+                    copyLink.addEventListener("click", function() {
+                        navigator.clipboard.writeText(anchorUrl)
+                    })
 
-                let innerDiv = document.createElement('div');
-                innerDiv.className = 'glfh_linkContainer';
-                innerDiv.appendChild(copyLink);
+                    let innerDiv = document.createElement('div');
+                    innerDiv.className = 'glfh_linkContainer';
+                    innerDiv.appendChild(copyLink);
 
-                clonedHeaderNode.appendChild(innerDiv);
+                    clonedHeaderNode.appendChild(innerDiv);
 
-                let outerDiv = document.createElement('div');
-                outerDiv.className = 'glfh_headerContainer';
-                outerDiv.appendChild(clonedHeaderNode);
+                    let outerDiv = document.createElement('div');
+                    outerDiv.className = 'glfh_headerContainer';
+                    header.parentElement.appendChild(outerDiv);
+                    outerDiv.appendChild(clonedHeaderNode);
 
-                header.parentNode.insertBefore(outerDiv, header);
-                header.remove();
-
-                document.getElementById(genlinkid)
-                        .addEventListener("click", function() {
-                            navigator.clipboard.writeText(anchorUrl)
-                        });
-            }
+                    header.replaceWith(outerDiv);
+                }
+            });
 });
